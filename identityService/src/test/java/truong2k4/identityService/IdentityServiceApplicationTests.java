750761ccbe3d27939b297f6c1f995c7540c2d37d
ken2k4 <truong2004ko@gmail.com>
2024-07-21 20:20:22 +0700

    Add IdentityService

---
+++ identityService/src/test/java/truong2k4/identityService/IdentityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package truong2k4.identityService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
